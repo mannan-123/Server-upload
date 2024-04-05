5f7fb4507146cd61f51f76fa3f96e49afc7b72fc
Abdul Mannan <98333068+mannan-123@users.noreply.github.com>
2024-04-05 23:07:53 +0500

    Add Server Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ServerUpload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblServer = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblUpload = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServer.Location = new System.Drawing.Point(41, 37);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(55, 20);
+            this.lblServer.TabIndex = 0;
+            this.lblServer.Text = "Server";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServer.Location = new System.Drawing.Point(130, 33);
+            this.txtServer.Multiline = true;
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(180, 29);
+            this.txtServer.TabIndex = 1;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(130, 74);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(180, 29);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(41, 78);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(83, 20);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(130, 114);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(180, 29);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Password";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.FlatAppearance.BorderSize = 8;
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(142, 162);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 32);
+            this.btnUpload.TabIndex = 6;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(45, 211);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(265, 23);
+            this.progressBar.TabIndex = 7;
+            // 
+            // lblUpload
+            // 
+            this.lblUpload.AutoSize = true;
+            this.lblUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpload.Location = new System.Drawing.Point(139, 250);
+            this.lblUpload.Name = "lblUpload";
+            this.lblUpload.Size = new System.Drawing.Size(90, 16);
+            this.lblUpload.TabIndex = 8;
+            this.lblUpload.Text = "Uploaded 0%";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 297);
+            this.Controls.Add(this.lblUpload);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.lblServer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server Upload";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblUpload;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace ServerUpload
+{
+    public partial class Form1 : Form
+    {
+
+        struct ftpSetting
+        {
+            public string fileName { get; set; }
+            public string server { get; set; }
+            public string username { get; set; }
+            public string password { get; set; }
+            public string fullName { get; set; }
+        }
+
+        ftpSetting inputParameter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblUpload.Text = "Upload Complete";
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblUpload.Text = $"Uploaded {e.ProgressPercentage}%";
+            progressBar.Value = e.ProgressPercentage;
+            progressBar.Update();
+        }
+
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string fileName = ((ftpSetting)e.Argument).fileName;
+            string fullName = ((ftpSetting)e.Argument).fullName;
+            string username = ((ftpSetting)e.Argument).username;
+            string password = ((ftpSetting)e.Argument).password;
+            string server = ((ftpSetting)e.Argument).server;
+
+        FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", server, fileName)));
+        request.Method = WebRequestMethods.Ftp.UploadFile;
+        request.Credentials = new NetworkCredential(username, password);
+        Stream ftpStream = request.GetRequestStream();
+        FileStream fs = File.OpenRead(fullName);
+        byte[] buffer = new byte[1024];
+        double total = (double)fs.Length;
+        int byteRead = 0;
+        double read = 0;
+        do
+        {
+            if (!backgroundWorker.CancellationPending)
+            {
+                byteRead = fs.Read(buffer, 0, 1024);
+                ftpStream.Write(buffer, 0, byteRead);
+                read += (double)byteRead;
+                double percentage = read / total * 100;
+                backgroundWorker.ReportProgress((int)percentage);
+            }
+        }
+
+        while (byteRead != 0);
+        fs.Close();
+        ftpStream.Close();
+    }
+    private void uploadOnServer()
+    {
+        // Start the file upload process by calling RunWorkerAsync and passing in the input parameter
+        backgroundWorker.RunWorkerAsync(inputParameter);
+
+    }
+
+    private void btnUpload_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "All files]|*.*" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo file = new FileInfo(ofd.FileName);
+                    inputParameter.username = txtUsername.Text;
+                    inputParameter.password = txtPassword.Text;
+                    inputParameter.server = txtServer.Text;
+                    inputParameter.fileName = file.Name;
+                    inputParameter.fullName = file.FullName;
+                }
+            }
+            uploadOnServer();
+        }
+
+    }
+}
